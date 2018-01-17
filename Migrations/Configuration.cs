1d7b641d125197f20a3a000073949d66f2ce8980
Ricky Hammonds <mail@rhammonds.com>
2018-01-17 15:14:12 -0500

    Add files via upload

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,54 @@
+namespace AspNetIdentity.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using AspNetIdentity.Infrastructure;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AspNetIdentity.Infrastructure.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        //protected override void Seed(AspNetIdentity.Infrastructure.ApplicationDbContext context)
+        //{
+        //    //  This method will be called after migrating to the latest version.
+
+        //    //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+        //    //  to avoid creating duplicate seed data. E.g.
+        //    //
+        //    //    context.People.AddOrUpdate(
+        //    //      p => p.FullName,
+        //    //      new Person { FullName = "Andrew Peters" },
+        //    //      new Person { FullName = "Brice Lambson" },
+        //    //      new Person { FullName = "Rowan Miller" }
+        //    //    );
+        //    //
+        //}
+
+        protected override void Seed(AspNetIdentity.Infrastructure.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "SuperPowerUser",
+                Email = "taiseer.joudeh@mymail.com",
+                EmailConfirmed = true,
+                FirstName = "Taiseer",
+                LastName = "Joudeh",
+                Level = 1,
+                JoinDate = DateTime.Now.AddYears(-3)
+            };
+
+            manager.Create(user, "MySuperP@ssword!");
+        }
+    }
+}
